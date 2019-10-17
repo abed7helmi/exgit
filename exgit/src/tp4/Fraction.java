61bb0c0517f2425d3e0a14b780fba529319b99e4
Helmi Abed <abed.helmi@yahoo.fr>
2019-10-17 10:35:09 +0200

    q1

---
+++ exgit/src/tp4/Fraction.java
@@ -0,0 +1,19 @@
+package tp4;
+
+public class Fraction {
+	private double numerateur , denominateur ;
+	
+	public Fraction(double numerateur , double denominateur)
+	{
+		this.denominateur=denominateur;
+		this.numerateur=numerateur;
+		
+	}
+	
+	public Fraction(double numerateur)
+	{
+		this.denominateur=1.0;
+		this.numerateur=numerateur;
+	}
+
+}
